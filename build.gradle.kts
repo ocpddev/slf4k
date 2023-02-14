24fdb2e89207c52b0e0917da22b114d1a86dc10e
Sola <sola@ocpd.dev>
2023-02-15 03:29:48 +0800

    build: dependency updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ repositories {
 
 dependencies {
     api(kotlin("stdlib-jdk8"))
-    api("org.slf4j:slf4j-api:2.0.5")
+    api("org.slf4j:slf4j-api:2.0.6")
     implementation(kotlin("reflect"))
     testImplementation("ch.qos.logback:logback-core:1.4.5")
     testImplementation("ch.qos.logback:logback-classic:1.4.5")
